3a8b9eb2f6a149a2d2c20057597007a15b802abb
Edwardkaanja <edwardkaranja072@gmail.com>
2025-09-25 07:36:43 +0300

    Add files via upload

---
+++ Volume.c
@@ -0,0 +1,22 @@
+/*
+Name:edward
+Reg no:CT101/26505/25
+Description: volume calculation 
+*/
+
+#include <stdio.h>
+
+int main(){
+
+float radius,height,pi;
+
+printf("Enter radius value:");
+scanf("%f",&radius);
+
+printf("\n Enter height value:");
+scanf("%f",&height);
+
+printf("\n Enter pi value:");
+scanf("%f",&pi);
+    return 0;
+}
