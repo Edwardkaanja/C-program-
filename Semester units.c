a1a9e9d65c4d20f7b33c2d2f3054d398843f5fc4
Edwardkaanja <edwardkaranja072@gmail.com>
2025-09-25 07:25:48 +0300

    Add files via upload

---
+++ Semester units.c
@@ -0,0 +1,10 @@
+//computer science units
+  int main()
+{ printf("introduction to programming and problem solving\n ") ;
+  printf("introduction to computer architecture\n ") ;
+  printf("operating systems 1\n");
+  printf("analogue electronics \n");
+  printf("discrete mathematics\n ");
+  printf("fundamentals of IT\n");
+  return 0 ; 
+  } 
